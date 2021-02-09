c71df6b18f1cd7dd4a36af2919823fa3827a8c17
Shubhofficial1 <Shubhofficial1@gmail.com>
2021-02-10 00:53:14 +0530

    first commit

---
+++ frontend/src/components/Header.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './Header.css'
+import SearchIcon from '@material-ui/icons/Search'
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket'
+const Header = () => {
+  return (
+    <nav className='header'>
+      <Link to='/'>
+        <img
+          className='header__logo'
+          src='http://pngimg.com/uploads/amazon/amazon_PNG11.png'
+          alt=''
+        ></img>
+      </Link>
+      <div className='header__search'>
+        <input type='text' className='header__searchInput'></input>
+        <SearchIcon className='header__searchIcon' />
+      </div>
+
+      <div className='header__nav'>
+        <Link to='/login' className='header__link'>
+          <div className='header__option'>
+            <span className='headerOptionLineOne'>Hello shubu</span>
+            <span className='headerOptionLineTwo'>Sign In</span>
+          </div>
+        </Link>
+      </div>
+
+      <div className='header__nav'>
+        <Link to='/' className='header__link'>
+          <div className='header__option'>
+            <span className='headerOptionLineOne'>Returns</span>
+            <span className='headerOptionLineTwo'> & Orders</span>
+          </div>
+        </Link>
+      </div>
+
+      <div className='header__nav'>
+        <Link to='/' className='header__link'>
+          <div className='header__option'>
+            <span className='headerOptionLineOne'>Your</span>
+            <span className='headerOptionLineTwo'>Prime</span>
+          </div>
+        </Link>
+      </div>
+
+      <Link to='/checkout' className='header__link'>
+        <div className='header__optionBasket'>
+          <ShoppingBasketIcon />
+          <span className='headerOptionLineTwo header__basketCount'>0</span>
+        </div>
+      </Link>
+    </nav>
+  )
+}
+
+export default Header
